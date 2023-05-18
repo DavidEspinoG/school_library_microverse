d3e64493b6fef5d690ea042a725311983e7cb3b9
David Espino <davidespinog@gmail.com>
2023-05-18 14:28:34 -0600

    Add menu in app.rb

--- app.rb
+++ app.rb
@@ -1,39 +1,58 @@
 class App
 
   def menu
-    puts "Welcome to School Library App!\nHola"
+    message =  "Welcome to School Library App!\n\n"
+    message << "Please choose an option by entering a number:\n"
+    message << "1 - List all books\n"
+    message << "2 - List all people\n"
+    message << "3 - Create a person\n"
+    message << "4 - Create a book\n"
+    message << "5 - Create a rental\n"
+    message << "6 - List all rentals for a given person id\n"
+    message << "7 - Exit\n"
   end
   def all_books 
     'All books'
   end
   def all_people 
-
+    'All people'
   end
   def create_person 
-
+    'Create person'
   end
   def create_book 
-
+    'Create book'
   end
   def create_rental
-
+    'Create rental'
   end
   def all_rentals(id)
-
+    'All rental'
   end
 
   def run 
     option = ''
-    if option == '7' 
-      puts 'Thanks for using this app!'
-    end
+    
     while option != '7'
       puts menu
       option = gets.chomp
-      if option == '7' 
+      case option
+      when '1'
+        puts all_books
+      when '2'
+        puts all_people
+      when '3'
+        puts create_person
+      when '4'
+        puts create_book
+      when '5'
+        puts create_rental
+      when '6'
+        puts all_rentals 1
+      when '7'
         puts 'Thanks for using this app!'
-      else 
-        puts "Your option is #{option}"
+      else
+        puts 'Not a valid option'
       end
     end  
   end
