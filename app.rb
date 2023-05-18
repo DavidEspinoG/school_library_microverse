40531fd838806686fcd0cc2884e775780e10f791
David Espino <davidespinog@gmail.com>
2023-05-18 14:41:05 -0600

    Refactor run method

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 class App
-
   def menu
-    message =  "Welcome to School Library App!\n\n"
+    message = "Welcome to School Library App!\n\n"
     message << "Please choose an option by entering a number:\n"
     message << "1 - List all books\n"
     message << "2 - List all people\n"
@@ -11,50 +10,55 @@ class App
     message << "6 - List all rentals for a given person id\n"
     message << "7 - Exit\n"
   end
-  def all_books 
+
+  def all_books
     'All books'
   end
-  def all_people 
+
+  def all_people
     'All people'
   end
-  def create_person 
+
+  def create_person
     'Create person'
   end
-  def create_book 
+
+  def create_book
     'Create book'
   end
+
   def create_rental
     'Create rental'
   end
-  def all_rentals(id)
+
+  def all_rentals(_id)
     'All rental'
   end
 
-  def run 
+  def run
     option = ''
-    
     while option != '7'
       puts menu
       option = gets.chomp
-      case option
-      when '1'
-        puts all_books
-      when '2'
-        puts all_people
-      when '3'
-        puts create_person
-      when '4'
-        puts create_book
-      when '5'
-        puts create_rental
-      when '6'
-        puts all_rentals 1
-      when '7'
-        puts 'Thanks for using this app!'
-      else
-        puts 'Not a valid option'
-      end
-    end  
+      puts 'Thanks for using this app' if option == '7'
+      select_option option
+    end
   end
 
+  def select_option(option)
+    case option
+    when '1'
+      puts all_books
+    when '2'
+      puts all_people
+    when '3'
+      puts create_person
+    when '4'
+      puts create_book
+    when '5'
+      puts create_rental
+    when '6'
+      puts all_rentals 1
+    end
+  end
 end
