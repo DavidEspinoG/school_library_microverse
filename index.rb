c581475c0b9923c8c58cb5889634ae9719131fdb
David Espino <davidespinog@gmail.com>
2023-05-18 10:27:13 -0600

    Add Classrom class

--- index.rb
+++ index.rb
@@ -1,9 +1,10 @@
-require_relative './person'
-require_relative './capitalize_decorator'
-require_relative './trimmer_decorator'
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
-puts capitalized_trimmed_person.correct_name
+require_relative './student'
+require_relative './classroom'
+
+student = Student.new(9, 'David', parent_permission: false)
+math_classroom = Classroom.new 'Math'
+
+math_classroom.add_student(student)
+
+puts student.classroom.label
+puts math_classroom.students
