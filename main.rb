32a0478db24ac8ffb92a18b818b6af695a2c1544
Jaime Villegas <jaimevillegas296@gmail.com>
2023-05-22 11:35:31 -0500

    Modify app.rb adding methods from main menu

--- main.rb
+++ main.rb
@@ -1,14 +1,8 @@
 require_relative './app.rb'
 
 class Main
-  # def initialize
-  #   @app = App.new
-  # end
-
   def initialize
-    @books = []
-    @people = []
-    @rentals = []
+    @app = App.new
   end
 
   def menu
@@ -50,102 +44,6 @@ class Main
     when '6'
       puts @app.all_rentals
     end
-  end
-
-  def get_permission(string)
-    return true if string.downcase == 'y'
-
-    false
-  end
-
-  def all_books
-    @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author} "
-    end
-    nil
-  end
-
-  def all_people
-    @people.each do |person|
-      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}"
-    end
-    nil
-  end
-
-  def create_person
-    print 'Do you want to create a student (1) or a teacher (2)?: '
-    option = gets.chomp
-    case option
-    when '1'
-      create_student
-    when '2'
-      create_teacher
-    end
-  end
-
-  def create_student
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-    print 'Has parent permission? [Y/N]: '
-    permission = gets.chomp
-    student = Student.new age, name, parent_permission: get_permission(permission)
-    @people.push(student)
-    nil
-  end
-
-  def create_teacher
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-    print 'Specialization: '
-    specialization = gets.chomp
-    teacher = Teacher.new age, specialization, name
-    @people.push(teacher)
-    nil
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new title, author
-    @books.push(book)
-    puts 'Book created successfully'
-  end
-
-  def create_rental
-    puts 'Select a book from the following list by number'
-    @books.each_with_index do |book, index|
-      puts "#{index})Title: #{book.title}, Author: #{book.author} "
-    end
-    book = @books[gets.chomp.to_i]
-    puts 'Select a person from the following list by number'
-    @people.each_with_index do |person, index|
-      puts "#{index}) #{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    person = @people[gets.chomp.to_i]
-    print 'Date (YYYY/MM/DD): '
-    date = gets.chomp
-    book.add_rental date, person
-
-    puts 'Rental created successfully'
-  end
-
-  def all_rentals
-    print 'ID of person: '
-    id = gets.chomp.to_i
-    filtered = @people.select { |person| person.id == id }
-    # binding.pry
-    puts "Person's name: #{filtered[0].name}"
-    filtered[0].rentals.each do |rental|
-      puts "Book name: #{rental.book.title}, date: #{rental.date}"
-    end
-
-    nil
   end
 
 end
